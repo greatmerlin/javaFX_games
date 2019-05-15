4111eb5208343cc3cb6af437d2a1c88f0cf83878
Theo <gmbaxevanos@gmx.ch>
2019-05-15 16:47:27 +0200

    Menu_added

--- Dame_Game/src/ffhs/controller/Main.java
+++ Dame_Game/src/ffhs/controller/Main.java
@@ -89,6 +89,7 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("../view/startGameMenu.fxml"));
             startLayout = loader.load();
+
             startGameMenuController = loader.getController();
             startGameMenuController.setObjects(this);
         }
